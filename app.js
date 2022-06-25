d6789dce2a63b8517c1dd153c8fe645a7407ca4d
Taloleamit <talole84@gmail.com>
2022-06-26 00:00:06 +0530

    update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ Port= 3000;
 
 app.get('/',(req,res)=>{
     res.json({
-        msg: 'hosting online by Amit github CI & CD tutorial'
+        msg: 'hosting online by Amit github CI & CD tutorial',
+        msg: "restart server"
     })
 })
 // create server
